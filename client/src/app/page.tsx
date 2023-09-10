c2c3f50f20a5a8e0b11466b5c69776565691e609
yash patel <yashpatel21102002@gmail.com>
2023-09-10 12:12:27 +0530

    yash-1009-problem solved in login/page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client"
 
 const LoginPage: React.FC = () => {
-  const getToken = ()=>{
-    const url = window.location.href;
-    console.log(url)
-  }
-  getToken()
+
   return (
     <div>
       <h1>This Is Middletube</h1>
